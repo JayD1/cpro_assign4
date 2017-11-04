06038c2e959aa2cab1b82d32df7f1d85bf4bc992
Jayadev Naram <31660846+JayD1@users.noreply.github.com>
2017-11-04 12:55:05 +0000

    vgjg

--- find_add.c
+++ find_add.c
@@ -1,11 +1,27 @@
-#include<stdio.h>
+                                                                                                         #include<stdio.h>
 #include<strings.h>
+#include<stdlib.h>
+
+int a=0;
 
 void find(char arr[],int i,int j,int n,int m)
 {
-	int k,loc[500],a=0;
+	int temp=1000000;
+	int k,loc[500000];
 	for(k=0;k<n*m;k++)if(arr[k]=='#')loc[a++]=k;
-	for(k=0;k<a;k++)printf("%d ",loc[k]);
+	if(a==0)
+	{
+		printf("-1\n");
+		return;
+	}
+	int x,y;
+	for(k=0;k<a;k++)
+	{
+		x=loc[k]/m;
+		y=loc[k]%m;
+		if(temp>abs(x-i)+abs(y-j))temp=abs(x-i)+abs(y-j);
+	}
+	printf("%d\n",temp);
 	return;
 }
 
@@ -17,7 +33,7 @@ int main()
 	for(k=0;k<n*m;k++)arr[k]='.';
 	int q;
 	scanf("%d",&q);
-	char inspec[4];
+	char inspec[5];
 	int i,j;
 	for(k=0;k<q;k++)
 	{
@@ -25,11 +41,11 @@ int main()
 		scanf("%d %d",&i,&j);
 		if(strncasecmp(inspec,"find",4)==0) find(arr,i-1,j-1,n,m);
 		else arr[(i-1)*m+(j-1)]='#';
-		//for(k=0;k<n*m;k++)
-		//{
-		//	printf("%c ",arr[k]);
-		//	if((k+1)%m==0)printf("\n");
-		//}
 	}
+	/*for(k=0;k<n*m;k++)
+	{
+		//if((k+1)%m==0&&k>0)printf("\n");
+		printf("%c ",arr[k]);
+	}*/
 	return 0;
 }
