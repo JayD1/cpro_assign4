a52adabde915ace7129e2b21386f1d2a40b1d306
Jayadev Naram <31660846+JayD1@users.noreply.github.com>
2017-11-06 14:57:53 +0000

    q4.c

--- seq.c
+++ seq.c
@@ -1,30 +1,24 @@
 #include<stdio.h>
 
-long long int arr[1001][1001];
-
-long long int select(int n,int k)
-{
-	if(n<k)return 0;
-	if(arr[n][k]!=-1)return arr[n][k];
-	if(n==k||k==0)return 1;
-	arr[n][k]=select(n-1,k)+select(n-1,k-1);
-	return arr[n][k];
-}
+long long m=1000000007;
 
 int main()
 {
-	int n,k,i,j;
+	int n,k,i,j,l;
 	scanf("%d %d",&n,&k);
-	for(i=0;i<1000;i++)
-		for(j=0;j<1000;j++)arr[i][j]=-1;
-	long long count=1;//since all 1s is always possible
-	for(i=2;i<n+1;i++)
+	long long count=0,a[k+1][n+1];
+	for(i=0;i<k+1;i++)
+		for(j=0;j<n+1;j++)a[i][j]=0;
+	for(j=1;j<n+1;j++)a[1][j]=1;// printf("%lld ",a[1][j]);}
+	for(i=2;i<k+1;i++)
 	{
-		if(i*k<=n){count+=select(n/i,k);
-		printf("%lld\n",count);}
-		if(i*(k-1)<=n){count+=select(n/i,k-1);
-		printf("%lld\n",count);}
+		for(j=1;j<n+1;j++)
+			for(l=j;l<n+1;l+=j)a[i][j]=(a[i][j]%m+a[i-1][l]%m)%m;
 	}
+	for(i=1;i<n+1;i++)count=(count%m+a[k][i]%m)%m;
+	/*for(i=0;i<k+1;i++){
+	for(j=0;j<n+1;j++)printf("%lld ",a[i][j]);
+	printf("\n");}*/
 	printf("%lld\n",count);
 	return 0;
 }
